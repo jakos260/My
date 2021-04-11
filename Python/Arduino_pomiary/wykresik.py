a97ca0286dd6fd8b6de5aac7d1a7a031dc9ed870
jakos260 <jankost21@gmail.com>
2021-04-12 00:36:03 +0200

    wykresy

--- Python/Arduino_pomiary/wykresik.py
+++ Python/Arduino_pomiary/wykresik.py
@@ -10,9 +10,11 @@
 
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 
 df = pd.read_excel(r'temp_rekord.xlsx', sheet_name='Dane')
 
+ticks = np.arange(0,1900,200)
 
 T = df['T']
 L = df['L']
@@ -24,17 +26,19 @@ color = 'tab:red'
 ax1.set_xlabel('czas')
 ax1.set_ylabel('T [˚C]', color=color)
 ax1.plot(H, T, color=color, label = 'T')
-# ax1.tick_params(axis='y', which='major', labelcolor=color)
-# ax1.grid(linewidth=0.9, color = 'black')
+ax1.tick_params(axis='y', which='major', labelcolor=color)
+ax1.grid(linewidth=0.9, color = 'black')
 
 ax2 = ax1.twinx()
 
 color = 'tab:blue'
 ax2.set_ylabel('L', color=color)
 ax2.plot(H, L, color=color, label = 'L')
-# ax2.tick_params(axis='y', labelcolor=color)
-# ax2.grid(linewidth=0.5, color = color)
+ax2.tick_params(axis='y', labelcolor=color)
+ax2.grid(linewidth=0.5, color = color)
+ax2.set_xticks(ticks)
+ax2.set_title('T(hour) & L(hour)', fontsize=14)
 
-fig.text(0.5, 0.98, '∆L = f(T) dla ZrO2', ha='center', va='center', fontsize=14)
+# fig.text(0.5, 0.98, 'jakiś napis', ha='center', va='center', fontsize=14)
 fig.tight_layout()
 plt.show()
--- Python/Docs/matplotlib/wykresy.py
+++ Python/Docs/matplotlib/wykresy.py
@@ -102,28 +102,29 @@ ax2 = ax1.twinx()
     ax2(blablabla)
 fig.tight_layout()
 """
-# ticks = np.arange(0,55,5) #generuje xticks'y
-#
-# fig, ax1 = plt.subplots(figsize=(12,7))
-#
-# ax1.set_xlabel('hour')
-# ax1.set_ylabel('T', color = 'red')
-# ax1.plot(h, t, color = 'r', label = 'T')
-# ax1.tick_params(axis='y', labelcolor = 'red')
-# # ax1.set_xscale('log') #rodzaj skali
-# ax1.grid(linewidth=1, color = 'black')
-#
-#
-# ax2 = ax1.twinx()
-# ax2.set_ylabel('L', color = 'blue')
-# ax2.plot(h, l, color = 'blue', label = 'L')
-# ax2.tick_params(axis='y', labelcolor = 'blue')
-# ax2.grid(linestyle='--')
-# ax2.set_xticks(ticks) # wystarczy ustawić dla ostatniego
-#
-# fig.text(0.5, 0.99, 'wykres podwójny', va='center', fontsize=14) #tytuł
-# fig.tight_layout()
-# plt.show()
+ticks = np.arange(0,55,5) #generuje xticks'y
+
+fig, ax1 = plt.subplots(figsize=(12,7))
+
+ax1.set_xlabel('hour')
+ax1.set_ylabel('T', color = 'red')
+ax1.plot(h, t, color = 'r', label = 'T')
+ax1.tick_params(axis='y', labelcolor = 'red')
+# ax1.set_xscale('log') #rodzaj skali
+ax1.grid(linewidth=1, color = 'black')
+
+
+ax2 = ax1.twinx()
+ax2.set_ylabel('L', color = 'blue')
+ax2.plot(h, l, color = 'blue', label = 'L')
+ax2.tick_params(axis='y', labelcolor = 'blue')
+ax2.grid(linestyle='--')
+ax2.set_xticks(ticks) # wystarczy ustawić dla ostatniego
+ax2.set_title('T(hour) & L(hour)') # tak samo tytuł
+
+fig.text(0.5, 0.99, 'wykres podwójny', va='center', fontsize=14) #tytuł
+fig.tight_layout()
+plt.show()
 
 """
 ______________dużo wykresów___________
@@ -135,22 +136,22 @@ Parametry:
 > sharex/sharey = współdzielenie osi x/y przez wykresy ['all','none','col','row']
 > squeeze = ...
 """
-ticks = np.arange(0,50,10) #generuje xticks'y
+# ticks = np.arange(0,50,10) #generuje xticks'y
 
-fig, ax = plt.subplots(nrows=2, ncols=2, sharey='row')
+# fig, ax = plt.subplots(nrows=2, ncols=2, sharey='row')
 
-ax[0,0].plot(h, t, 'r')
-ax[0,0].set_xticks(ticks)
+# ax[0,0].plot(h, t, 'r')
+# ax[0,0].set_xticks(ticks)
 
-ax[0,1].plot(h, t, 'b')
-ax[0,1].set_xticks(ticks)
+# ax[0,1].plot(h, t, 'b')
+# ax[0,1].set_xticks(ticks)
 
-ax[1,0].plot(h, l, 'k')
-ax[1,0].set_xticks(ticks)
+# ax[1,0].plot(h, l, 'k')
+# ax[1,0].set_xticks(ticks)
 
-ax[1,1].plot(h, l, 'g')
-ax[1,1].set_xticks(ticks)
+# ax[1,1].plot(h, l, 'g')
+# ax[1,1].set_xticks(ticks)
 
-fig.suptitle('kilka wykresów')
+# fig.suptitle('kilka wykresów')
 
-plt.show()
+# plt.show()
