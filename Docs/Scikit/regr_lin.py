7640df3d4c3ddf32b24c5adb085718134819e0ae
jakos260 <jankost21@gmail.com>
2021-04-30 15:57:45 +0200

    poly_reg

--- Docs/Scikit/regr_lin.py
+++ Docs/Scikit/regr_lin.py
@@ -9,35 +9,34 @@ import numpy as np
 from sklearn import datasets, linear_model
 from sklearn.metrics import mean_squared_error, r2_score
 
-# Load the diabetes dataset
-diabetes_X, diabetes_y = datasets.load_diabetes(return_X_y=True)
 
-# Używaj tylko jednej funkcji
-diabetes_X = diabetes_X[:, np.newaxis, 2]
+X, y = datasets.load_diabetes(return_X_y=True) 
+# return_X_y >>> True - return data, target; False - return bunch(data, target)
+X = X[:, np.newaxis, 2]
 
 # Podział danych na zestawy uczące / testowe
-diabetes_X_train = diabetes_X[:-20]
-diabetes_X_test = diabetes_X[-20:]
+X_train = X[:-20]
+X_test = X[-20:]
 
 # Podział celi na zestawy treningowe / testowe
-diabetes_y_train = diabetes_y[:-20]
-diabetes_y_test = diabetes_y[-20:]
+y_train = y[:-20]
+y_test = y[-20:]
 
 # tworzenie modelu
 model = linear_model.LinearRegression()
 
 # trenowanie modelu
-model.fit(diabetes_X_train, diabetes_y_train)
+model.fit(X_train, y_train)
 
 # utworzenie y przewidywanych
-diabetes_y_pred = model.predict(diabetes_X_test)
+y_pred = model.predict(X_test)
 
 
 print('współczynniki: \n', model.coef_)
-print('Mean squared error: %.2f' % mean_squared_error(diabetes_y_test, diabetes_y_pred))
-print('współczynnik determinacji: %.2f' % r2_score(diabetes_y_test, diabetes_y_pred)) # 1 oznacza idealne dopasowanie
+print('Mean squared error: %.2f' % mean_squared_error(y_test, y_pred))
+print('współczynnik determinacji: %.2f' % r2_score(y_test, y_pred)) # 1 oznacza idealne dopasowanie
 
 plt.figure(figsize=(12,6))
-plt.scatter(diabetes_X_test, diabetes_y_test,  color='k')
-plt.plot(diabetes_X_test, diabetes_y_pred, color='b', linewidth=2)
+plt.scatter(X_test, y_test,  color='k')
+plt.plot(X_test, y_pred, color='b', linewidth=2)
 plt.show()
--- Docs/Scikit/regr_poly.py
+++ Docs/Scikit/regr_poly.py
@@ -12,40 +12,51 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn import datasets
 from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import make_pipeline
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 
 # Load the diabetes dataset
-x, y = datasets.load_diabetes(return_X_y=True)
-x = x[:, np.newaxis, 2]
+boston = datasets.load_boston()
+df = pd.DataFrame(boston['data'], columns=boston['feature_names'])
+df = pd.concat([df, pd.Series(boston['target'], name='MEDV')], axis=1)
+X = df.iloc[:, [4,7]].values
+y = df.iloc[:, 4].values
+print(X.shape,'\n',y.shape)
 
-# Podział danych na zestawy uczące / testowe
-x_train = x[:-20]
-x_test = x[-20:]
+from sklearn.model_selection import train_test_split 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-# Podział celi na zestawy treningowe / testowe
-y_train = y[:-20]
-y_test = y[-20:]
-
-# regresja liniowa dla porównania
+# print(x_test, '\n____', y_test)
+# ____________regresja liniowa dla porównania___________
 lin_reg = LinearRegression()
-lin_reg.fit(x_train, y_train)
+lin_reg.fit(X, y)
+y_pred_lin = lin_reg.predict(X)
 
-# nasz model wielomianowy
-poly_model =  PolynomialFeatures(degree = 10) #degree - stopień wielomianu
-x_poly = poly_model.fit_transform(x_train)
+# __________nasz model wielomianowy____________
 
-lin_reg_2 = LinearRegression()
-lin_reg_2.fit(x_poly, y_train)
+ # degree - stopień wielomianu
+degree = 5
+# poly_reg =  PolynomialFeatures(degree) # degree można też zdefiniować tu (5)
+# x_transform = poly_reg.fit_transform(x)
+
+# lin_reg_2 = LinearRegression()
+# lin_reg_2.fit(x_transform, y) 
+
 
 
 fig, (ax1, ax2) = plt.subplots(2)
-fig.suptitle('regresja wielomianowa')
+ax1.scatter(X, y, color='k', marker='.')
+ax1.plot(X, y_pred_lin, 'y')
+ax1.set_title('regresja liniowa')
 
-ax1.scatter(x_test, y_test, color='r')
-ax1.plot(x_test, lin_reg.predict(x_test), 'b')
-
-ax2.scatter(x_test, y_test, color='r')
-ax2.plot(x_test, lin_reg_2.predict(poly_model.fit_transform(x_test)), color='b')
-
+# ax2.scatter(x.iloc[:,0], y, color='k')
+# ax2.plot(x, lin_reg_2.predict(x), color='g')
+# ax2.plot(x, lin_reg_2.predict( poly_reg.fit_transform(x)), color='g')
+ax2.set_title('regresja wielomianowa stopnia ' + str(degree))
 plt.show()
+
+# print("Wielomian\nŚredni błąd kwadratowy: ", np.mean((poly_regr.predict(x) - y) ** 2))
+# print('współczynniki: ', lin_reg_2.intercept_, lin_reg_2.coef_)
+# print("F liniowa\nŚredni błąd kwadratowy: ", np.mean((lin_reg.predict(x) - y) ** 2))
+
