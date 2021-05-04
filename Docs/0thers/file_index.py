732fe9c59ec90fb4f84460535dac756ce02641a3
jakos260 <jankost21@gmail.com>
2021-05-04 14:25:19 +0200

    file_index

--- Docs/0thers/file_index.py
+++ Docs/0thers/file_index.py
@@ -1,16 +1,19 @@
 import numpy as np
 import pandas as pd
 import datetime
+import os
 
-def name()
-    name = 'data\DF_' + str(datetime.date.today())
-    return 
+def name():
+    i = 1
+    while os.path.exists(f'DF_{i}_{str(datetime.date.today())}.csv'):
+        i += 1
+    return f'DF_{i}_{str(datetime.date.today())}.csv'
 
 def to_csv(df, overwrite):
     if overwrite:
-        df.to_csv(name())
+        df.to_csv(f'DF_{str(datetime.date.today())}.csv')
     else:
         df.to_csv(name())
 
-df = np.random.random((2, 5))
+df = pd.DataFrame(np.random.randint(1, 99, (1, 4)))
 to_csv(df, overwrite=True)
---
+++ Docs/Numpy/random.py
@@ -0,0 +1,14 @@
+'''
+rand(d0, d1, …, dn)	Random values in a given shape.
+randn(d0, d1, …, dn)	Return a sample (or samples) from the “standard normal” distribution.
+randint(low[, high, size, dtype])	Return random integers from low (inclusive) to high (exclusive).
+random_integers(low[, high, size])	Random integers of type np.int between low and high, inclusive.
+random_sample([size])	Return random floats in the half-open interval [0.0, 1.0).
+random([size])	Return random floats in the half-open interval [0.0, 1.0).
+ranf([size])	Return random floats in the half-open interval [0.0, 1.0).
+sample([size])	Return random floats in the half-open interval [0.0, 1.0).
+choice(a[, size, replace, p])	Generates a random sample from a given 1-D array
+bytes(length)	Return random bytes.
+'''
+
+import numpy as np
