e1c3887ecea39304af02bd65b12f01733ad7de51
jakos260 <jankost21@gmail.com>
2021-07-22 12:16:14 +0200

    commit

---
+++ kursy/NVIDIA_ML/evaluate_models.py
@@ -0,0 +1,25 @@
+# import the required modules/libraries.
+# This should be a self-sufficient method.
+
+# function to compute the f1_score of the two bounding boxes
+def f1_score(A, B):
+    """
+    Return the constrained model index
+
+    :param A:                Coordinates of the first bounding box in the following format:
+                             yminA, xminA, ymaxA, xmaxA
+                             
+                             
+                             
+    :param B:                Coordinates of the second bounding box in the following format:
+                             xminB, yminB, xmaxB, ymaxB
+
+    :return:                 returns an integer indicating the F1 score
+    """
+    result = -1 
+    
+    ##TODO##
+    # implement the function logic here
+    
+    
+    return result
