3a06ee0600bbb319214951a58bae1cb60f913f18
jakos260 <jankost21@gmail.com>
2022-04-26 23:46:24 +0200

    add signal plot builder

--- SymulatorEP/plotting.py
+++ SymulatorEP/plotting.py
@@ -8,17 +8,25 @@ import plotly.graph_objects as go
 dataPath = "sygnay"
 dataFrames = os.listdir(dataPath)
 
-fig2 = make_subplots(rows=12, cols=1)
+fig2 = make_subplots(rows=3, cols=1)
 fig2.update_layout(title_text="ECG")
 i = 0
 for df in dataFrames:
-    if(df[0] == 'V'):
-        V = pd.read_csv(os.path.join(dataPath, df), header=None)
-        V_y = np.concatenate((np.zeros(len(V[0])), np.array(V[0]), np.zeros(len(V[0])*8)), axis=None)
-        V_x = np.linspace(0, len(V_y)/2000, len(V_y))    
-        i+=1
-        
-        fig2.add_trace(go.Line(x=V_x, y=V_y, name=df), row=i, col=1)
+    name = os.path.splitext(df)[0].split("-")
+    
+    V = pd.read_csv(os.path.join(dataPath, df), header=None)
+    V_y = np.concatenate((np.zeros(len(V[0])), np.array(V[0]), np.zeros(len(V[0])*8)), axis=None)
+    V_x = np.linspace(0, len(V_y)/2000, len(V_y))
+    if(name[0] == 'I' and name[-1] == 'v2'):
+        fig2.add_trace(go.Line(x=V_x, y=V_y, name=df), row=1, col=1)
+    if(name[0] == 'II' and name[-1] == 'v2'):
+        fig2.add_trace(go.Line(x=V_x, y=V_y, name=df), row=2, col=1)
+    if(name[0] == 'VI' and name[-1] == 'v2'):
+        fig2.add_trace(go.Line(x=V_x, y=V_y, name=df), row=3, col=1)
+    # if(name[0] == 'I' and name[-1] == 'v2'):
+    #     fig2.add_trace(go.Line(x=V_x, y=V_y, name=df), row=1, col=1)
 
-fig2.write_html('ECGsygnay.html', auto_open=True)
-# fig2.show()
+
+
+# fig2.write_html('ECGsygnay.html', auto_open=True)
+fig2.show()
